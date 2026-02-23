d049614d32cb7d6dc383da4c414712e016f8a261
さわい <fennecin@gmail.com>
2026-02-23 06:40:38 +0100

    Fix Cross-Origin Isolation headers

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'coi-v2';
+const CACHE = 'coi-v3';
 
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
